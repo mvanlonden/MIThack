fd70b6511b130fd38ce115200af69faddaac442a
prescottprue <prescottprue@hotmail.com>
2014-03-15 14:49:15 -0400

    Logout works now.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -24,59 +24,69 @@ angular.module('myApp.controllers', [])
    }])
 
    .controller('LoginCtrl', ['$scope', 'loginService', '$location', function($scope, loginService, $location) {
-      $scope.email = null;
-      $scope.pass = null;
-      $scope.confirm = null;
-      $scope.createMode = false;
+        $scope.email = null;
+        $scope.pass = null;
+        $scope.name = null;
+        $scope.confirm = null;
+        $scope.createMode = false;
+        $scope.drop = true;
 
-      $scope.login = function(cb) {
-         $scope.err = null;
-         if( !$scope.email ) {
-            $scope.err = 'Please enter an email address';
-         }
-         else if( !$scope.pass ) {
-            $scope.err = 'Please enter a password';
-         }
-         else {
-            loginService.login($scope.email, $scope.pass, function(err, user) {
-               $scope.err = err? err + '' : null;
-               if( !err ) {
-                  cb && cb(user);
-               }
-            });
-         }
-      };
+        $scope.login = function(cb) {
+            $scope.err = null;
 
-      $scope.createAccount = function() {
-         $scope.err = null;
-         if( assertValidLoginAttempt() ) {
-            loginService.createAccount($scope.email, $scope.pass, function(err, user) {
-               if( err ) {
-                  $scope.err = err? err + '' : null;
-               }
-               else {
-                  // must be logged in before I can write to my profile
-                  $scope.login(function() {
-                     loginService.createProfile(user.uid, user.email);
-                     $location.path('/account');
-                  });
-               }
-            });
-         }
-      };
+            if( !$scope.email ) {
+                $scope.err = 'Please enter an email address';
+            }
+            else if( !$scope.pass ) {
+                $scope.err = 'Please enter a password';
+            }
+            else {
+                $scope.drop = false;
+                $('.dropdown-toggle').dropdown('toggle');
+                loginService.login($scope.email, $scope.pass,function(err, user) {
+                    $scope.err = err? err + '' : null;
+                    if( !err ) {
+                        cb && cb(user);
+                    }
+                });
+            }
+        };
 
-      function assertValidLoginAttempt() {
-         if( !$scope.email ) {
-            $scope.err = 'Please enter an email address';
-         }
-         else if( !$scope.pass ) {
-            $scope.err = 'Please enter a password';
-         }
-         else if( $scope.pass !== $scope.confirm ) {
-            $scope.err = 'Passwords do not match';
-         }
-         return !$scope.err;
-      }
+        $scope.createAccount = function() {
+            $scope.err = null;
+            if( assertValidLoginAttempt() ) {
+                loginService.createAccount($scope.email, $scope.pass, function(err, user) {
+                    if( err ) {
+                        console.log(err);
+                        $scope.err = err? err + '' : null;
+                    }
+                    else {
+                        // must be logged in before I can write to my profile
+                        $scope.login(function() {
+                            loginService.createProfile(user.uid, user.email, $scope.name);
+                            $location.path('/account');
+                        });
+                    }
+                });
+            }
+        };
+
+        function assertValidLoginAttempt() {
+            if( !$scope.email ) {
+                $scope.err = 'Please enter an email address';
+            }
+            else if( !$scope.pass ) {
+                $scope.err = 'Please enter a password';
+            }
+            else if( $scope.pass !== $scope.confirm ) {
+                $scope.err = 'Passwords do not match';
+            }
+            return !$scope.err;
+        };
+
+        $scope.logout = function() {
+            loginService.logout();
+        };
    }])
 
    .controller('AccountCtrl', ['$scope', 'loginService', 'changeEmailService', 'firebaseRef', 'syncData', '$location', 'FBURL', function($scope, loginService, changeEmailService, firebaseRef, syncData, $location, FBURL) {
